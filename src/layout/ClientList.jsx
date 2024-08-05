ea2492d40c4de716c670c742a4d11806c5c29c03
Fabian F <noobseca123@hotmail.com>
2024-08-05 15:48:43 -0500

    version 0.3

--- src/layout/ClientList.jsx
+++ src/layout/ClientList.jsx
@@ -1,39 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import clientesData from '../data/cliente.json'; // Asegúrate de que la ruta sea correcta
+import clientesData from '../data/cliente.json';
 import clienteImagen from '../assets/client.png';
 
 const ClientList = () => {
   const [clientes, setClientes] = useState([]);
+  const [selectedCliente, setSelectedCliente] = useState(null);
 
   useEffect(() => {
-    // Simular la carga de datos desde un archivo JSON
     setClientes(clientesData);
   }, []);
 
+  const handleClienteClick = (codigo) => {
+    setSelectedCliente(selectedCliente === codigo ? null : codigo);
+  };
+
   return (
-    <div className="fixed overflow-y-auto p-6 bg-gray-100 max-h-full min-h-screen border-r-4 w-56 ">
+    <div className="fixed overflow-y-auto p-6 bg-gray-100 max-h-full min-h-screen border-r-4 w-56 group">
       <h1 className="text-3xl font-bold mb-6 text-center">Clientes</h1>
-      <div className="space-y-6">
+      <div className="space-y-8 group-hover:space-y-16 mt-12">
         {clientes.map((cliente) => (
-          <div key={cliente.codigo} className="relative mb-4 p-4 w-28 h-32 z-2 font-bold text-gray-700  bg-white shadow-md rounded group mx-auto hover:h-44">
-            <img src={clienteImagen} alt={cliente.codigo} className='w-20 mx-auto' />
+          <div
+            key={cliente.codigo}
+            className={`relative p-4 w-28 h-32 z-0 font-bold text-gray-700 bg-white shadow-md rounded mx-auto transition-all `}
+            onClick={() => handleClienteClick(cliente.codigo)}
+          >
+            <img src={clienteImagen} alt={cliente.codigo} className="w-20 mx-auto" />
             <h2 className="text-base font-semibold mb-2 text-center">{cliente.codigo}</h2>
 
-            <div className='absolute hidden group-hover:block z-10 top-0 w-48 -translate-x-10 h-auto left-0 p-1 pb-4 rounded-xl bg-white border-4 ' >
-              <p className="text-gray-600 mb-2 text-center">Hora: {cliente.horaLlegada}</p>
-              <ul className="space-y-1 mb-2">
-                {cliente.productos.map((producto, index) => (
-                  <li key={index} className="flex justify-between p-2  bg-gray-50 rounded">
-                    <span>{producto.nombre}</span>
-                    <span>${producto.precio.toFixed(2)}</span>
-                  </li>
-                ))}
-              </ul>
-              <div className="flex justify-between font-semibold border-4 p-1 rounded-xl">
-                <span>Total:</span>
-                <span>${cliente.valorAcumulado.toFixed(2)}</span>
+            {selectedCliente === cliente.codigo && (
+              <div className="absolute z-10 -top-4 left-1/2 transform -translate-x-1/2 w-48 h-52 overflow-auto p-4 bg-white border-4 shadow-lg rounded-xl">
+                <p className="text-gray-600 mb-2 text-center bg-red-100 ">Hora: {cliente.horaLlegada}</p>
+                <ul className="space-y-1 mb-2">
+                  {cliente.productos.map((producto, index) => (
+                    <li key={index} className="flex justify-between p-2 bg-gray-50 rounded">
+                      <span>{producto.nombre}</span>
+                      <span>${producto.precio.toFixed(2)}</span>
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex justify-between font-semibold border-4 p-1 rounded-xl">
+                  <span>Total:</span>
+                  <span>${cliente.valorAcumulado.toFixed(2)}</span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         ))}
       </div>
--- src/layout/MesaList.jsx
+++ src/layout/MesaList.jsx
@@ -4,30 +4,40 @@ import mesasData from '../data/mesas.json';
 
 const MesaList = () => {
   const [mesas, setMesas] = useState([]);
+  const [selectedMesa, setSelectedMesa] = useState(null);
 
   useEffect(() => {
-    // Simular la carga de datos desde un archivo JSON
     setMesas(mesasData);
   }, []);
 
+  const handleMesaClick = (codigo) => {
+    setSelectedMesa(selectedMesa === codigo ? null : codigo);
+  }
+
   return (
-    <div className="p-6 bg-gray-100 h-full">
-      <h2 className="text-2xl font-bold mb-6">Lista de Mesas</h2>
-      <div className="grid grid-cols-3 gap-8 my-12">
+    <div className="p-6 bg-gray-100 h-full w-full">
+      <h2 className="text-2xl font-bold mb-6 text-center">Lista de Mesas</h2>
+      <div className="grid grid-cols-3 max-w-screen-xl mx-auto gap-8 my-12">
         {mesas.map((mesa) => (
-          <div key={mesa.codigo} className='relative group border rounded-xl'>
-            <img src={MesaImagen} alt="" className='w-28 mx-auto place-items-center items-center justify-center' /> 
-            <div className="hidden group-active:block p-4 bg-white shadow-md rounded ">
-              <h3 className="text-xl font-semibold">Mesa {mesa.codigo}</h3>
-              <p><strong>Hora Ocupado:</strong> {new Date(mesa.horaOcupado).toLocaleString()}</p>
-              <p><strong>Valor Acumulado:</strong> ${mesa.valorAcumulado.toFixed(2)}</p>
-              <h4 className="mt-2 font-semibold">Productos:</h4>
-              <ul className="list-disc list-inside ml-4">
-                {mesa.productos.map((producto, index) => (
-                  <li key={index}>{producto.nombre} - ${producto.precio.toFixed(2)}</li>
-                ))}
-              </ul>
-            </div>
+          <div key={mesa.codigo} className='relative border rounded-xl'>
+            <button className='flex items-center justify-center w-full' onClick={() => handleMesaClick(mesa.codigo)}>
+              <img src={MesaImagen} alt="" className='w-28 mx-auto' />
+            </button>
+
+            {selectedMesa === mesa.codigo && (
+              <div className="p-4 bg-white shadow-md rounded ">
+                <h3 className="text-xl font-semibold text-center bg-orange-300">Mesa {mesa.codigo}</h3>
+                <p><strong>Hora Ocupado:</strong> {new Date(mesa.horaOcupado).toLocaleString()}</p>
+                <p><strong>Valor Acumulado:</strong> ${mesa.valorAcumulado.toFixed(2)}</p>
+                <h4 className="mt-2 font-semibold">Productos:</h4>
+                <ul className="list-disc list-inside ml-4">
+                  {mesa.productos.map((producto, index) => (
+                    <li key={index}>{producto.nombre} - ${producto.precio.toFixed(2)}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+
             <div className="absolute top-4 left-1/2 transform -translate-x-1/2 font-bold">{mesa.codigo}</div>
           </div>
         ))}
