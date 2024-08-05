801b45c8641a921e843046fc0ad67aeb90d4d450
Fabian F <noobseca123@hotmail.com>
2024-08-05 11:44:30 -0500

    version 0.3

--- src/components/Caja.jsx
+++ src/components/Caja.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CajaRegistradora from "../assets/cashRegister.png";
 import Money from "../assets/money.png";
 import Inventory from "../assets/inventory.png";
@@ -6,14 +6,21 @@ import Client from "../assets/client.png";
 
 
 function Caja() {
+
+  const [isMenuVisibe, setIsMenuVisible] = useState(false);
+
+  const toogleMenu = () => {
+    setIsMenuVisible(!isMenuVisibe);
+  }
+
   return (
     <div>
-      <div className="relative flex my-2.5 bg-white items-center justify-center "><img src={CajaRegistradora} alt="" className="size-20" />
-        <div className="absolute bg-white translate-y-20 mt-20 w-[450px] h-44 border-4 rounded-xl p-2 z-20">
+      <div className="relative flex my-2.5 bg-white items-center justify-center "><button onClick={toogleMenu}><img src={CajaRegistradora} alt="" className="size-20" /></button>
+        <div className={`${isMenuVisibe ? 'block' : 'hidden'} absolute bg-white translate-y-20 mt-20 w-[450px] h-44 border-4 rounded-xl p-2 z-20`}>
           <div className="grid grid-cols-3 gap-4 font-bold text-sm text-center">
-            <div className="border-4 p-1 rounded-xl"><img src={Client} alt="pago" className="size-24 mx-auto" /><span >Agregar Cliente</span></div>
-            <div className="border-4 p-1 rounded-xl"><img src={Money} alt="pago" className="size-24 mx-auto" /><span >Efectuar Pago</span></div>
-            <div className="border-4 p-1 rounded-xl"><img src={Inventory} alt="pago" className="size-24 mx-auto" /><span >Agregar Inventario</span></div>
+            <div className="border-4 p-1 rounded-xl hover:scale-110 duration-200"><img src={Client} alt="pago" className="size-24 mx-auto" /><span >Agregar Cliente</span></div>
+            <div className="border-4 p-1 rounded-xl hover:scale-110 duration-200"><img src={Money} alt="pago" className="size-24 mx-auto" /><span >Efectuar Pago</span></div>
+            <div className="border-4 p-1 rounded-xl hover:scale-110 duration-200"><img src={Inventory} alt="pago" className="size-24 mx-auto" /><span >Agregar Inventario</span></div>
           </div>
         </div>
       </div>
