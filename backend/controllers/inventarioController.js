fb5e4c26da94d611ba8ea72c05d2093ed6567218
Fabian F <noobseca123@hotmail.com>
2024-09-15 22:24:40 -0500

    version 1.1

--- backend/controllers/inventarioController.js
+++ backend/controllers/inventarioController.js
@@ -39,12 +39,19 @@ router.post('/pagos', async (req, res) => {
       // Obtener el último código
       const ultimoCodigo = pagos.length > 0 ? pagos[pagos.length - 1].codigo : 0;
       const nuevoCodigo = ultimoCodigo + 1; // Autoincrementa el último código
+
+      const optionsFecha = { year: 'numeric', month: '2-digit', day: '2-digit' };
+      const optionsHora = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+  
+      // Fecha y hora en la zona horaria de Colombia
+      const fecha = new Date().toLocaleDateString('es-CO', optionsFecha); // Fecha actual en formato colombiano
+      const hora = new Date().toLocaleTimeString('es-CO', optionsHora);   // Hora actual en formato colombiano
   
       const nuevoPago = {
         codigo: nuevoCodigo, // Asigna el nuevo código
         empresa: empresa || '',
-        fecha: new Date().toLocaleDateString(), // Formato de fecha
-        hora: new Date().toLocaleTimeString(), // Formato de hora
+        fecha: fecha, // Formato de fecha
+        hora: hora, // Formato de hora
         productos,
         valorPago: (costoTotal * -1)
       };
