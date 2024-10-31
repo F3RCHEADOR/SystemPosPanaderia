2b94a55507725b293fcda2825a2cc85f51496c86
F3RCHEADOR <66806022+F3RCHEADOR@users.noreply.github.com>
2024-10-31 12:22:24 -0500

    Update listProducts.jsx

--- src/layout/listProducts.jsx
+++ src/layout/listProducts.jsx
@@ -14,6 +14,7 @@ const ListProducts = ({ clientData, isEdit }) => {
     fetch(backend+'api/categorias')
       .then(response => response.json())
       .then(data => {
+        console.log(data);
         if (data && Array.isArray(data)) {
           setCategorias(data);
 
